85d07a19de46f2bfae23a58c3881d1d4bfa70f5c
Alok Sharma <alok98sharma@gmail.com>
2023-10-26 20:48:20 +0530

    My first commit for swift poc

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Tab from "../../ReactComponents/Tabs/Tab";
+import  './Home.css';
+
+const Home=()=>{
+    return(
+        <div className="homeContainer">
+          <Tab />
+        </div>
+    )
+}
+
+export default Home;
